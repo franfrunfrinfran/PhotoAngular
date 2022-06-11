730ef8967fa71c9aaf0fa5276e7f1977594f3f50
Fran Rodriguez <javierufo91@gmail.com>
2022-06-11 13:54:40 +0200

    Se agrega modulo de Cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import path from "path";
+import cors from 'cors';
 
 
 const app = express();
@@ -14,13 +15,14 @@ app.set('port', process.env.PORT || 3000);
 //MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.use('/api',photoRoutes);
 app.use('/api/auth', authRoutes);
 
 //Folder where will be used to store public files
-app.use('/uploads',express.static(path.resolve('../uploads')))
-
+app.use('/uploads',express.static(path.resolve('uploads')))
+//app.use('/uploads', express.static('uploads'))
 
 export default app;
